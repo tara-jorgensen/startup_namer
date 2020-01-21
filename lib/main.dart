9f0d5bb91c68ef3ccdc637b9af78aabcd8c78850
Tara Jørgensen <traarj@gmail.com>
2020-01-21 14:16:55 +0100

    added final variables

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,9 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
